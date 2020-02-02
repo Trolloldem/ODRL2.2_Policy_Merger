e1b32d83dcc670792bcd096c91ff3dacb5b346df
oldem <g.oldani@studenti.unibg.it>
2020-02-02 12:03:06 +0100

    Migliorata la procedura di intersezione delle policy(da tempo dipendente dal numero di regole a tempo costante=numero azioni nel vocabolario)

--- src/Policy/Set.java
+++ src/Policy/Set.java
@@ -5,6 +5,8 @@ import Rule.Prohibition;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import Rule.RuleTree;
 import Actions.Action;
@@ -66,6 +68,45 @@ public class Set implements Policy {
     @Override
     public Set IntersectWith(Policy p) {
         Set res=null;
+        if(p instanceof Set) {
+            ArrayList<Rule> resRuleList = new ArrayList<Rule>();
+            RuleTree thisUseTree = this.getUseTree();
+            RuleTree pUseTree = ((Set) p).getUseTree();
+            RuleTree thisTransferTree = this.getTransferTree();
+            RuleTree pTransferTree = ((Set) p).getTransferTree();
+
+            ConcurrentHashMap<Action, String> thisStates = thisUseTree.getAllStates();
+            ConcurrentHashMap<Action, String> pStates = pUseTree.getAllStates();
+            for (Map.Entry<Action, String> entry : thisStates.entrySet()) {
+
+                if (pStates.get(entry.getKey()).equals("Prohibited") || entry.getValue().equals("Prohibited")) {
+                    resRuleList.add(new Prohibition(entry.getKey()));
+                    continue;
+                }
+                if (pStates.get(entry.getKey()).equals("Undefined") || entry.getValue().equals("Undefined")) {
+                    continue;
+                }
+                resRuleList.add(new Permission(entry.getKey()));
+            }
+            thisStates = thisTransferTree.getAllStates();
+            pStates = pTransferTree.getAllStates();
+            for (Map.Entry<Action, String> entry : thisStates.entrySet()) {
+
+                if (pStates.get(entry.getKey()).equals("Prohibited") || entry.getValue().equals("Prohibited")) {
+                    resRuleList.add(new Prohibition(entry.getKey()));
+                    continue;
+                }
+                if (pStates.get(entry.getKey()).equals("Undefined") || entry.getValue().equals("Undefined")) {
+                    continue;
+                }
+                resRuleList.add(new Permission(entry.getKey()));
+            }
+            resRuleList.addAll(this.getListProhib());
+            resRuleList.addAll(((Set) p).getListProhib());
+            res = new Set(resRuleList);
+        }
+        return res;
+       /** Set res=null;
         ArrayList<Rule> resRuleList = new ArrayList<Rule>();
         ArrayList<Action> thisPermittedAction=new ArrayList<>();
         ArrayList<Action> pPermittedAction=new ArrayList<>();
@@ -73,7 +114,7 @@ public class Set implements Policy {
         /**
          * Per entrambe le policy da intersecare si estrae una lista dei permessi più inclusivi da loro presentati,
          * il procedimento è ripetuto per entrambi gli alberi dei permessi
-         */
+
         if(p instanceof Set){
             for(Rule r : RuleList){
                 if(r instanceof Permission){
@@ -110,7 +151,7 @@ public class Set implements Policy {
             /**
              * Per ogni permesso di entrambe le policy, si controlla se l'altra policy permette la stessa azione o
              * solamente un subset di questa
-             */
+
             for(Action a : thisPermittedAction){
                 List<Action> listChild=Arrays.asList(a.getIncludedBy());
                 for(Action sec: pPermittedAction){
@@ -140,7 +181,7 @@ public class Set implements Policy {
             resRuleList.addAll(((Set) p).getListProhib());
             res = new Set(resRuleList);
         }
-        return res;
+        return res;**/
     }
 
     /**
