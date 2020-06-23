78e7872e7ee31fb2ecbcd61b04d02fb4fd640612
trolloldem <g.oldani@studenti.unibg>
2020-06-23 19:24:06 +0200

    Documentazione esempi scenari

--- src/test/java/runScenario.java
+++ src/test/java/runScenario.java
@@ -250,4 +250,61 @@ public class runScenario {
         Map<String, AssetCollection> hier = mergingResult.getRight();
         documentProducer.produceDocument(resTree,hier,"./src/test/java/outputs/union1-2.ttl");
     }
+
+    @Test
+    public void  simpleTest(){
+        String examplePath = "./src/test/java/single1.jsonld";
+        Map<AssetCollection, List<Rule>> mappa = policyReader.readPolicyRules(examplePath);
+        Asset every = new Asset("EveryAsset");
+        Map<String,Asset> assets = policyReader.readAssets(examplePath);
+
+        for(Map.Entry<String,Asset> entry : assets.entrySet()) {
+
+            if (entry.getValue().getParents().size() == 0)
+                entry.getValue().addParent(every);
+        }
+
+        assets.put("EveryAsset", every);
+
+        AssetTree tree = new AssetTree(every);
+
+        for(Map.Entry<AssetCollection,List<Rule>> entry : mappa.entrySet()){
+            if(entry.getValue()!=null && entry.getValue().size() >0 ){
+
+                Policy actPolicy = new Set(entry.getValue(),assets.get(entry.getKey().getURI()));
+                tree.setPolicy(actPolicy,false);
+            }
+        }
+
+
+        String secondPath = "./src/test/java/single2.jsonld";
+        Map<AssetCollection, List<Rule>>  mappaSecond = policyReader.readPolicyRules(secondPath);
+        Map<String,Asset>assetsSecond = policyReader.readAssets(secondPath);
+        Asset everySecond = new Asset("EveryAsset");
+
+        for(Map.Entry<String,Asset> entry : assetsSecond.entrySet()) {
+            if (entry.getValue().getParents().size() == 0)
+                entry.getValue().addParent(everySecond);
+        }
+
+        assetsSecond.put("EveryAsset", everySecond);
+
+        AssetTree treeSecond = new AssetTree(everySecond);
+
+        for(Map.Entry<AssetCollection,List<Rule>> entry : mappaSecond.entrySet()){
+            if(entry.getValue()!=null && entry.getValue().size() >0 ){
+
+                Policy actPolicy = new Set(entry.getValue(),assetsSecond.get(entry.getKey().getURI()));
+
+                treeSecond.setPolicy(actPolicy,false);
+            }
+        }
+
+
+        Pair<AssetTree, Map<String, AssetCollection>> mergingResult = merger.union(assets,assetsSecond,tree,treeSecond);
+
+        AssetTree resTree = mergingResult.getLeft();
+        Map<String, AssetCollection> hier = mergingResult.getRight();
+        documentProducer.produceDocument(resTree,hier,"./src/test/java/outputs/single.ttl");
+    }
 }
